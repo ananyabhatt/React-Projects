1a4424819f9b2c69e00ee53f446ca1db59558ac7
Ananya Bhatt <ananyasbhatt@gmail.com>
2026-03-12 17:36:13 +0000

    move CoreConcepts component into components folder

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,15 +1,6 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header";
-
-function CoreConcepts({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import CoreConcepts from "./components/CoreConcepts";
 
 function App() {
   return (
